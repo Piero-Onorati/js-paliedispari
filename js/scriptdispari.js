e4efd497fd102f12628d9234074793de704955d8
Piero <piero.onorati@virgilio.it>
2021-06-07 16:59:04 +0200

    add correction

--- js/scriptdispari.js
+++ js/scriptdispari.js
@@ -1,12 +1,10 @@
 /* L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
 
-// ---------- FUNCTIONS ---------- //
-
 function getRnd(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-function pairOdispari(number) {
+function pariOdispari(number) {
     if (number % 2 == 0) {
         return 'pari';
     } else {
@@ -14,36 +12,35 @@ function pairOdispari(number) {
     }    
 }
 
-
-// ---------- INPUT ---------- //
+// ------------------ INPUT ------------------- //
 
 // Utente sceglie se pari o dispari
 var pariDispari = prompt('Ciao Utente, scegli tra "pari" o "dispari"');
 
 // Utente sceglie un numero
-var user = prompt('Perfetto! Ora scegli un numero da 1 a 5');
+var user = parseInt(prompt('Perfetto! Ora scegli un numero da 1 a 5'));
 
 // Controllo su numero utente
 while (user < 0 || user > 5) {
-    user = prompt('Attenzione! Scegliere un numero da 1 a 5');
+    user = parseInt(prompt('Attenzione! Scegliere un numero da 1 a 5'));
 }
 
 // Numero random generato dal computer
 var computer = getRnd (1,5);
 
 
-// --------- ELABORATION --------- //
+// ------------------ ELABORATION ------------------- //
 
 var somma = user + computer;
 
 
-// ---------- OUTPUT ---------- //
+// ------------------ OUTPUT ------------------- //
 
 document.getElementById('numeroutente').innerHTML = user;
 
 document.getElementById('numeroPC').innerHTML = computer;
 
-if (pairOdispari(somma) == pariDispari ) {
+if (pariOdispari(somma) == pariDispari) {
     document.getElementById("esito").innerHTML = 'Hai vinto!!!';
 }else{
     document.getElementById("esito").innerHTML = 'Hai perso';
